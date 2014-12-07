23c1af372639d6627c5d04679fa0d768825d430a
hfcorriez <hfcorriez@gmail.com>
2014-12-07 22:45:23 +0800

    Change schema

--- app/migrations/schema.sql
+++ app/migrations/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `posts` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `url` varchar(1000) DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `url` varchar(1000) NOT NULL DEFAULT '',
   `content` text,
-  `digg_count` int(11) DEFAULT NULL,
-  `view_count` int(11) DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
+  `digg_count` int(11) DEFAULT '0',
+  `view_count` int(11) DEFAULT '0',
+  `status` tinyint(4) DEFAULT '0',
+  `updated_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
