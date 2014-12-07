169a994bb3c83cddb941274dd3389792b287e006
hfcorriez <hfcorriez@gmail.com>
2014-12-08 00:04:15 +0800

    Scape page with limit size http socket size

--- app/src/Api/PostUrlCheck.php
+++ app/src/Api/PostUrlCheck.php
@@ -15,19 +15,46 @@ class PostUrlCheck extends \Api
             $this->error('Params "url" required');
         }
 
-        $opts = array(
-            'http' => array(
-                'timeout' => 5
-            )
-        );
+        $_payload = $this->scrapePage($url);
 
-        $context = stream_context_create($opts);
-        $payload = file_get_contents($url, false, $context);
-
-        preg_match('/<title>([^<]+)<\/title>/i', $payload, $title_matches);
-        preg_match('/<meta name="description" content="(.*)" \/> /i', $payload, $description_matches);
+        preg_match('/<title>([^<]+)<\/title>/i', $_payload, $title_matches);
+        preg_match('/<meta name="description" content="(.*)" \/> /i', $_payload, $description_matches);
 
         $this->data['title'] = $title_matches[1];
         $this->data['content'] = $description_matches[1];
+    }
+
+    /**
+     * Scrape page with read limit size socket
+     *
+     * @param $url
+     * @return string
+     */
+    private function scrapePage($url)
+    {
+        $urlParts = parse_url(trim($url));
+
+        $fp = fsockopen($urlParts['host'], $urlParts['scheme'] === 'https' ? '443' : '80', $errNo, $errStr, 30);
+        $_payload = '';
+
+        if (!$fp) {
+            $this->error('Scrape error');
+        } else {
+            $out = "GET {$urlParts['path']} HTTP/1.1\r\n";
+            $out .= "Host: {$urlParts['host']}\r\n";
+            $out .= "Connection: Close\r\n\r\n";
+            fwrite($fp, $out);
+            while (!feof($fp) && strlen($_payload) < 2048) {
+                $_payload .= fgets($fp, 128);
+            }
+            fclose($fp);
+        }
+
+        preg_match('/Location: (.+)/i', $_payload, $redirect_matches);
+        if ($redirect_matches[1]) {
+            return $this->scrapePage($redirect_matches[1]);
+        }
+
+        return $_payload;
     }
 }
