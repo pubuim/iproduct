9aaba1b6d1930643daee069a0615bd71f605ab29
Yan Zhu <picturepan2@hotmail.com>
2014-12-07 18:29:44 +0800

    主体结构

--- app/views/ui.php
+++ app/views/ui.php
@@ -1,45 +1,75 @@
 <div id="page">
     <header class="header">
         <div class="wrapper">
-            <nav class="navbar" role="navigation">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-menu">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <div class="brand pull-left">
-                        <h1 class="site-title">iProduct</h1>
-                        <h2 id="tagline" class="site-title tagline">Inspired by Product Hunt</h2>
+            <div class="row">
+                <nav class="navbar col-lg-12" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-menu">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="brand pull-left">
+                            <h1 class="site-title">iProduct</h1>
+                            <h2 id="tagline" class="site-title tagline">Inspired by Product Hunt</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="collapse navbar-collapse" id="navbar-collapse-menu">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">关于</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">菜单 <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">提交</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
-    <div id="content" class="i-content">
-        <div class="wrapper">
-            <div id="desc">
-                Pagon is a modern PHP framework, It's under MIT license.
+                    <div class="collapse navbar-collapse" id="navbar-collapse-menu">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">关于</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">菜单 <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">提交</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
-    </div>
+    </header>
+    <article class="content">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div id="day-141201" class="day day-1 day-container">
+                        <div class="day-title">
+                            <h4 class="day-date-title">今天</h4>
+                            <time class="day-day-title">14-12-1</time>
+                        </div>
+                        <div class="day-content">
+                            <ul class="posts">
+                                <li class="post">
+                                    <div class="post-item-vote">
+                                        <i class="icon glyphicon-arrow-up"></i>
+                                        <div class="vote-num">12</div>
+                                    </div>
+                                    <div class="post-item-main">
+                                        <div class="main-title">瀑布</div>
+                                        <div class="main-tagline description">测试</div>
+                                    </div>
+                                    <div class="post-item-user">
+                                        <img id="user-123" class="avatar" src="/images/doge.png" />
+                                    </div>
+                                    <div class="post-item-comments">
+                                        12
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="day-more">加载更多</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
     <footer class="footer">
         <div class="wrapper">
             <div class="copyright">
