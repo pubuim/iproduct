11ad6c75890b9d40eb61a1b09013e20058f81dad
Yan Zhu <picturepan2@hotmail.com>
2014-12-07 18:03:33 +0800

    header 初步实现

--- app/views/ui.php
+++ app/views/ui.php
@@ -1,13 +1,50 @@
-<div id="wrapper" class="typo typo-selection">
-    <h1>Welcome to Pagon!</h1>
-
-    <div id="tagline">I'm come from future!</div>
-
-    <div id="desc">
-        Pagon is a modern PHP framework, It's under MIT license.
+<div id="page">
+    <header class="header">
+        <div class="wrapper">
+            <nav class="navbar" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <div class="brand pull-left">
+                        <h1 class="site-title">iProduct</h1>
+                        <h2 id="tagline" class="site-title tagline">Inspired by Product Hunt</h2>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-collapse-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">关于</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">菜单 <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">提交</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <div id="content" class="i-content">
+        <div class="wrapper">
+            <div id="desc">
+                Pagon is a modern PHP framework, It's under MIT license.
+            </div>
+        </div>
     </div>
-
-    <div id="action">
-        <a href="http://ipagon.com" class="button" target="_blank">Visit documentation!</a>
-    </div>
+    <footer class="footer">
+        <div class="wrapper">
+            <div class="copyright">
+                &copy; 2014
+            </div>
+        </div>
+    </footer>
 </div>
