fdcceb16d0a69f67ba4a3e0a206e7b1907f878ea
Yan Zhu <picturepan2@hotmail.com>
2014-12-08 00:19:45 +0800

    单条样式

--- app/views/ui.php
+++ app/views/ui.php
@@ -48,18 +48,26 @@
                             <ul class="posts">
                                 <li class="post">
                                     <div class="post-item-vote">
-                                        <i class="icon glyphicon-arrow-up"></i>
-                                        <div class="vote-num">12</div>
+                                        <a href="#3" class="vote-up">
+                                            <i class="vote-icon glyphicon glyphicon-chevron-up"></i>
+                                            <div class="vote-num">123</div>
+                                        </a>
+                                    </div>
+                                    <div class="post-item-meta">
+                                        <div class="post-item-user">
+                                            <img id="user-123" class="avatar" src="/images/doge.png" title="User 123"/>
+                                        </div>
+                                        <div class="post-item-comments">
+                                            <i class="comment-icon glyphicon glyphicon-comment"></i>
+                                            <span>123</span>
+                                        </div>
                                     </div>
                                     <div class="post-item-main">
-                                        <div class="main-title">瀑布</div>
+                                        <div class="main-title">
+                                            <a href="#2">瀑布</a>
+                                        </div>
                                         <div class="main-tagline description">测试</div>
-                                    </div>
-                                    <div class="post-item-user">
-                                        <img id="user-123" class="avatar" src="/images/doge.png" />
-                                    </div>
-                                    <div class="post-item-comments">
-                                        12
+                                        <a href="#1" class="post-link"></a>
                                     </div>
                                 </li>
                             </ul>
