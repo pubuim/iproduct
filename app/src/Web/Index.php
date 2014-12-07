1fe175c5798410fcb7837f3f14d9a134e7bee0ba
hfcorriez <hfcorriez@gmail.com>
2014-12-07 20:50:51 +0800

    输出最新的几天新闻

--- app/src/Web/Index.php
+++ app/src/Web/Index.php
@@ -3,23 +3,44 @@
 namespace Web;
 
 use Model\Model;
+use Moment\Moment;
 
 class Index extends \Web
 {
     /**
      * @var \Model\Post
      */
-    protected $_post;
+    protected $_postModel;
 
-    public $posts;
+    public $posts = array();
 
+    /**
+     * GET /
+     */
     public function get()
     {
-        $this->posts = $this->_post->find_many();
+        $date_range = 5;
+
+        for ($i = 0; $i < $date_range; $i++) {
+            $_m = new Moment();
+            $_s = $_m->subtractDays($i)->startOf('day')->getTimestamp();
+            $_e = $_m->addDays(1)->startOf('day')->getTimestamp();
+
+            /**
+             * PostsF
+             */
+            $this->posts[$_s] = $this->_postModel
+                ->where_gte('created_at', $_s)
+                ->where_lt('created_at', $_e)
+                ->order_by_desc('digg_count')
+                ->limit(10)
+                ->find_many();
+        }
+
         $this->render('index.php');
     }
 
-    public function post()
+    public function postModel()
     {
         print_r($this->input->data);
     }
@@ -30,6 +51,6 @@ class Index extends \Web
     protected function before()
     {
         $this->loadOrm();
-        $this->_post = Model::factory('Post');
+        $this->_postModel = Model::factory('Post');
     }
 }
--- app/views/index.php
+++ app/views/index.php
@@ -1,7 +1,10 @@
 <div id="wrapper" class="typo typo-selection">
-    <ul>
-        <?php foreach ($posts as $post): ?>
-            <li><?php echo $post->title ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <?php foreach ($posts as $time => $list): ?>
+        <h5><?php echo date('Y-m-d', $time); ?></h5>
+        <ul>
+            <?php foreach ($list as $post): ?>
+                <li><?php echo $post->title ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endforeach; ?>
 </div>
