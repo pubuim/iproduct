40915d9098918fcb768530b02d95b2b55f572b3e
hfcorriez <hfcorriez@gmail.com>
2014-12-21 15:42:39 +0800

    提取 post-day 为加载更多做准备

--- app/views/index.php
+++ app/views/index.php
@@ -3,22 +3,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <?php foreach ($posts as $time => $list): ?>
-                    <div id="day-<?php echo date('Ymd', $time); ?>" class="day day-1 day-container">
-                        <!-- day-1 为今天起第几天 -->
-                        <div class="day-title">
-                            <h4 class="day-date-title">今天</h4>
-                            <time class="day-day-title"><?php echo date('Y-m-d', $time); ?></time>
-                        </div>
-                        <div class="day-content">
-                            <ul class="posts">
-                                <?php foreach ($list as $post): ?>
-                                    <?php include(__DIR__ . '/blocks/post-item.php'); ?>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                        <div class="posts-more"><a href="#5">查看其余 x 个产品</a></div>
-                        <!-- 大于 10 条显示，x 替换成：总数 - 10，链接为显示所有链接 -->
-                    </div>
+                    <?php include(__DIR__ . '/blocks/post-day.php'); ?>
                 <?php endforeach; ?>
             </div>
         </div>
