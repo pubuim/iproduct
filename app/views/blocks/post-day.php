c9454f96007c7c90ae93567718ca3f410f1e878f
hfcorriez <hfcorriez@gmail.com>
2014-12-21 16:37:06 +0800

    今天的判断和时间的处理

--- app/views/blocks/post-day.php
+++ app/views/blocks/post-day.php
@@ -1,27 +1,28 @@
-<div id="day-<?php echo date('Ymd', $time); ?>" class="day day-1 day-container">
+<?php $today = date('Y-m-d'); ?>
+<div id="day-<?php echo $time; ?>" class="day day-1 day-container">
     <!-- day-1 为今天起第几天 -->
     <div class="day-title">
-        <h4 class="day-date-title">今天</h4>
-        <time class="day-day-title posts-day"><?php echo date('Y-m-d', $time); ?></time>
+        <h4 class="day-date-title"><?php echo $today === $time ? '今天' : $time; ?></h4>
+        <time class="day-day-title posts-day"><?php echo $time; ?></time>
     </div>
     <?php if ($list): ?>
-    <div class="day-content">
-        <ul class="posts">
-            <?php foreach ($list as $post): ?>
-                <li class="post-container">
-                    <?php include(__DIR__ . '/post-item.php'); ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <?php if (count($list)>10): ?>
-    <div class="posts-more"><a href="#5">查看其余 <?php echo count($list) - 10 ?> 个产品</a></div>
-    <?php endif; ?>
-    <!-- 大于 10 条显示，x 替换成：总数 - 10，链接为显示所有链接 -->
+        <div class="day-content">
+            <ul class="posts">
+                <?php foreach ($list as $post): ?>
+                    <li class="post-container">
+                        <?php include(__DIR__ . '/post-item.php'); ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+        <?php if (count($list) > 10): ?>
+            <div class="posts-more"><a href="#5">查看其余 <?php echo count($list) - 10 ?> 个产品</a></div>
+        <?php endif; ?>
+        <!-- 大于 10 条显示，x 替换成：总数 - 10，链接为显示所有链接 -->
     <?php else: ?>
-    <div class="post-empty">
-        <i class="empt-icon glyphicon glyphicon-link"></i>
-        <span class="post-empty-text">没有内容</span>
-    </div>
+        <div class="post-empty">
+            <i class="empt-icon glyphicon glyphicon-link"></i>
+            <span class="post-empty-text">没有内容</span>
+        </div>
     <?php endif; ?>
 </div>
